a5a3e76f5a33667483de37f3b68fc93581a8332c
tknickman <tom.knickman@vercel.com>
2024-01-16 17:01:30 -0500

    Docs

---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
